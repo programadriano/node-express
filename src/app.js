3c89cc7b2326f1c381d565a003061bfaa17d413d
Thiago Adriano <thiago.adriano26@gmail.com>
2018-05-27 10:56:48 -0300

    add body-parser

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ const router = express.Router();
 const index = require('./routes/index');
 const personRoute = require('./routes/personRoute');
 
-
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.use('/', index);
 app.use('/person', personRoute);
