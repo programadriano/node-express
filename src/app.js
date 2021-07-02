704a3cfec399fc90a8827f670fa0499354c987ac
Thiago da Silva Adriano <tadriano.net@gmail.com>
2021-07-02 13:55:51 -0300

    removendo o app.use(express.json())

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-
-const router = express.Router();
 
 //Rotas
 const index = require('./routes/index');
 const personRoute = require('./routes/personRoute');
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/', index);
 app.use('/person', personRoute);
